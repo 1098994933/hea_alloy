0a41448280218e37bf32183d217e475d4d164daa
zyj <1283255024@qq.com>
2024-12-02 16:35:42 +0800

    update 2024-12-2 Add binary classification

--- 3_ml_phase_cls.py
+++ 3_ml_phase_cls.py
@@ -28,7 +28,7 @@ val_config = {}
 
 
 def read_data():
-    dataset = pd.read_csv("./data/1_phase_ml_dataset.csv")
+    dataset = pd.read_csv("./data/1_phase_ml_dataset.csv")  # 最后一列是相信息，前面是成分特征
     Y_col = 'Phase'
     features = list(dataset.columns[:-1])
     return dataset, Y_col, features
@@ -47,7 +47,7 @@ def read_magpie_data():
 
 def read_alloy_data():
     """
-    使用23个特征
+    使用23个合金特征
     :return:
     """
     dataset = pd.read_csv("./data/2_phase_alloy_feature.csv")
@@ -71,33 +71,41 @@ if __name__ == '__main__':
     X = scaler.fit_transform(X)
     Y = pd.read_csv("./data/1_phase_ml_dataset.csv")[Y_col]
 
+    # 为二分类重构目标
+    binary_classification = True
+    if binary_classification:  # 改为二分类
+        for i in range(len(Y)):
+            if Y.iloc[i] != 'FCC':
+                Y.iloc[i] = 'NOT_FCC'
+
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
                                                         test_size=0.2,
-                                                        random_state=0)
+                                                        random_state=0)  # 要对FCC预测效果好
 
-    print(X_train.shape, Y_train.shape)
+    # print(X_train.shape, Y_train.shape)
     X_train = pd.DataFrame(X_train, columns=features)
     columns_with_nulls = X_train.columns[X_train.isnull().any()]
     print(columns_with_nulls)
-    feature_selection = SelectKBest(f_classif, k=len(features)).fit(X_train, Y_train)
-
+    k_sample = 10
+    # 原代码：选所有magpie特征feature_selection = SelectKBest(f_classif, k=len(features)).fit(X_train, Y_train)
+    feature_selection = SelectKBest(f_classif, k=k_sample).fit(X_train, Y_train)
     feature_scores = feature_selection.scores_
-    print('feature_scores:', feature_scores)
-    indices = np.argsort(feature_scores)[::-1]
-    val_config['feature_num'] = len(features)
+    # print('feature_scores:', feature_scores)
+    indices = np.argsort(feature_scores)[::-1] # -1转升序为降序，返回索引
+    # 原代码：用所有magpie特征建模 val_config['feature_num'] = len(features)
+    val_config['feature_num'] = k_sample  # 一个字典
     best_features = list(X_train.columns.values[indices[0:val_config['feature_num']]])
-    print("best_features", best_features)
-    X_train = feature_selection.transform(X_train)
+    # print("best_features", best_features)
+    X_train = feature_selection.transform(X_train)  # 提取筛选出来的k个特征？
     X_test = feature_selection.transform(X_test)
-    sc = MinMaxScaler()
     alg_dict = {
         # "LogisticRegression": LogisticRegression(),
         # "KNeighbors": KNeighborsClassifier(),
         # "DecisionTree": DecisionTreeClassifier(),
-        # "RandomForest": RandomForestClassifier(n_estimators=100, min_samples_split=2, random_state=4, n_jobs=4),
-        "ExtraTrees": ExtraTreesClassifier(
-            **{'max_depth': 5, 'min_samples_split': 6, 'n_estimators': 133}),
-        # "GradientBoosting": GradientBoostingClassifier(),
+        # "RandomForest": RandomForestClassifier(n_estimators=100, min_samples_split=2, random_state=4, n_jobs=-1),
+        # "ExtraTrees": ExtraTreesClassifier(
+        #     **{'max_depth': 5, 'min_samples_split': 6, 'n_estimators': 133}),
+        "GradientBoosting": GradientBoostingClassifier(),
         # "AdaBoost": AdaBoostClassifier(),
         # "SVC": SVC(),
     }
@@ -121,19 +129,20 @@ if __name__ == '__main__':
     # dataset['Y_predict'] = best_y_predict
     class_labels = list(set(Y_test))
     cm = confusion_matrix(Y_test, best_y_predict, labels=class_labels)
-    print(best_y_predict)
     # plot confusion matrix
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=class_labels, yticklabels=class_labels)
-    plt.xlabel('Actual')
-    plt.ylabel('Predicted')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
     plt.title('Confusion Matrix')
     plt.savefig(f'./figures/confusion_matrix.png', bbox_inches='tight')
+    plt.show()
 
     # use best model to predict
-    model_final = best_model
-    model_final.fit(X, Y)
-    dataset_predict = pd.read_csv("./data/2_oxidation_magpie_feature.csv")
-    y_predict = model_final.predict(scaler.transform(dataset_predict[features]))
-    dataset_predict.insert(0, "phase_predict", y_predict)
-    dataset_predict.to_csv("./data/3_oxidation_phase_predict.csv", index=False)
+    # model_final = best_model
+    # model_final.fit(X, Y)
+    # dataset_predict = pd.read_csv("./data/2_oxidation_slope_magpie_feature.csv")
+    # y_predict = model_final.predict(scaler.transform(dataset_predict[features]))
+    # print(y_predict)
+    # dataset_predict.insert(0, "phase_predict", y_predict)
+    # dataset_predict.to_csv("./data/3_oxidation_phase_predict.csv", index=False)
